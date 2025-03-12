f83aa3a68f26ba95671aea941aacccec7c445073
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-12 07:59:55 +0000

    update

--- scanner/app/tests/test_custom_logging.py
+++ scanner/app/tests/test_custom_logging.py
@@ -1,23 +1,16 @@
-from logging import DEBUG, INFO
 from os import environ
 from unittest.mock import patch
 
 import pytest
-from structlog import get_logger
 
 from app.custom_logging import set_up_custom_logging
 
 
-@pytest.mark.parametrize(
-    ("debug_env", "expected_level"), [("false", INFO), ("true", DEBUG)]
-)
+@pytest.mark.parametrize(("debug_env"), [("false"), ("true")])
 @patch.dict(environ, {}, clear=True)
-def test_set_up_custom_logging(debug_env: str, expected_level: int) -> None:
+def test_set_up_custom_logging(debug_env: str) -> None:
     """Test the set_up_custom_logging function."""
     # Arrange
     environ["INPUT_DEBUG"] = debug_env
     # Act
     set_up_custom_logging()
-    logger = get_logger()
-    # Assert
-    assert logger._logger.level == expected_level
