6bd9f85d7f7bc4f2b8ca763c5fe1c4765d1ff229
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-14 20:52:05 +0100

    update

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { SidebarProvider } from "./components/ui/sidebar";
 import "./index.css";
-import { SidebarProvider } from "./components/ui/sidebar";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
