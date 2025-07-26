f940d7763297ec99a34d4adfe3c966f9c0bcea28
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-26 22:46:33 +0100

    update

--- scanner/app/__main__.py
+++ scanner/app/__main__.py
@@ -7,7 +7,7 @@ from structlog import get_logger, stdlib
 from .analysis import run_analyser
 from .configuration import Configuration
 from .custom_logging import set_up_custom_logging
-from .custom_types import AnalysedRepository, Output, Total
+from .custom_types import AnalysedRepository, Output, Summary, Total
 
 logger: stdlib.BoundLogger = get_logger()
 
@@ -43,10 +43,10 @@ def generate_output(analysis: list[AnalysedRepository]) -> None:
         repositories=[
             {
                 "name": repository.name,
-                "summary": {
-                    "lines": repository.summary.total_line_count,
-                    "files": repository.summary.total_file_count,
-                },
+                "summary": Summary(
+                    lines=repository.summary.total_line_count,
+                    files=repository.summary.total_file_count,
+                ),
                 "commits": repository.commits,
             }
             for repository in analysis
