be6135b25c459571a599ef431410034aa3951ad8
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-26 21:45:09 +0100

    update

--- scanner/app/tests/test__main__.py
+++ scanner/app/tests/test__main__.py
@@ -2,13 +2,9 @@ from unittest.mock import MagicMock, patch
 
 from scanner.app.__main__ import (
     AnalysedRepository,
-    Configuration,
-    analyse_repository_files,
     clean_up,
     generate_output,
     main,
-    run_analyser,
-    timeline_analysis,
 )
 
 FILE_PATH = "scanner.app.__main__"
@@ -30,95 +26,6 @@ def test_main(
     mock_set_up_custom_logging.assert_called_once_with()
     mock_configuration.assert_called_once_with()
     mock_run_analyser.assert_called_once_with(mock_configuration.return_value)
-
-
-@patch(f"{FILE_PATH}.generate_output")
-@patch(f"{FILE_PATH}.timeline_analysis")
-@patch(f"{FILE_PATH}.analyse_repository_files")
-@patch(f"{FILE_PATH}.clone_repo")
-@patch(f"{FILE_PATH}.retrieve_repositories")
-@patch(f"{FILE_PATH}.ProjectSummary")
-def test_run_analyser(
-    mock_project_summary: MagicMock,
-    mock_retrieve_repositories: MagicMock,
-    mock_clone_repo: MagicMock,
-    _mock_analyse_repository_files: MagicMock,
-    mock_timeline_analysis: MagicMock,
-    mock_generate_output: MagicMock,
-) -> None:
-    """Test the run_analyser function."""
-    # Arrange
-    mock_configuration = MagicMock(spec=Configuration)
-    repository_mock = MagicMock(owner=MagicMock(login="owner"))
-    mock_retrieve_repositories.return_value = [repository_mock]
-    mock_clone_repo.return_value = "cloned_repositories/repo"
-    mock_project_summary.return_value = MagicMock()
-
-    # Act
-    run_analyser(mock_configuration)
-
-    # Assert
-    mock_retrieve_repositories.assert_called_once_with(mock_configuration)
-    mock_clone_repo.assert_called_once_with("owner", repository_mock.name)
-    mock_generate_output.assert_called_once_with(
-        [
-            {
-                "name": repository_mock.name,
-                "summary": mock_project_summary.return_value,
-                "commits": mock_timeline_analysis.return_value,
-            }
-        ]
-    )
-
-
-@patch(f"{FILE_PATH}.ProjectSummary")
-@patch(f"{FILE_PATH}.SourceAnalysis")
-@patch(f"{FILE_PATH}.Path")
-def test_analyse_repository_files(
-    mock_path: MagicMock,
-    mock_source_analysis: MagicMock,
-    mock_project_summary: MagicMock,
-) -> None:
-    """Test the analyse_repository_files function."""
-    # Arrange
-    folder_path = "test_folder"
-    repository_name = "test_repo"
-    mock_root = MagicMock()
-    mock_root.parts = ("root",)
-    mock_root.__str__.return_value = "root"
-    mock_path.return_value.walk.return_value = [(mock_root, [], ["file.py"])]
-    # Act
-    analyse_repository_files(mock_project_summary, folder_path, repository_name)
-    # Assert
-    mock_path.assert_called_once_with(folder_path)
-    mock_source_analysis.from_file.assert_called_once_with(
-        str(mock_root.__truediv__("file.py")), repository_name
-    )
-    mock_project_summary.add.assert_called_once_with(
-        mock_source_analysis.from_file.return_value
-    )
-
-
-@patch(f"{FILE_PATH}.ProjectSummary")
-@patch(f"{FILE_PATH}.analyse_repository_files")
-@patch(f"{FILE_PATH}.Repo")
-def test_timeline_analysis(
-    mock_repo: MagicMock,
-    mock_analyse_repository_files: MagicMock,
-    mock_project_summary: MagicMock,
-) -> None:
-    """Test the timeline_analysis function."""
-    # Arrange
-    commit = MagicMock()
-    mock_repo.return_value.iter_commits.return_value = [commit]
-    folder_path = "test_folder"
-    repository_name = "test_repo"
-    # Act
-    timeline_analysis(folder_path, repository_name)
-    # Assert
-    mock_analyse_repository_files.assert_called_once_with(
-        mock_project_summary.return_value, folder_path, repository_name
-    )
 
 
 @patch(f"{FILE_PATH}.Path")
