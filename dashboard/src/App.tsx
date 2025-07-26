21d9435f5e8094d8764091750e982f2435b2dce7
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-26 15:12:23 +0100

    Fix Dashboard Build (#310)
    # Pull Request
    
    ## Description
    
    This pull request introduces a minor type safety improvement in the
    `App.tsx` file. The change ensures that the `selectedRepo` state is
    explicitly typed as `RepoStats | null`.
    
    * Type safety improvement:
    *
    [`dashboard/src/App.tsx`](diffhunk://#diff-a55633d330003a77c3c0d08667db3da7cee2f90404fc82e51c45029ee6855db5R7-R11):
    Updated the `useState` hook for `selectedRepo` to use the type
    `RepoStats | null` for better type safety.

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -4,10 +4,11 @@ import RepositoryStatsDataPane from "@/components/repository-stats-data-pane";
 import { useState } from "react";
 import OverviewStatsDataPane from "./components/overview-stats-data-pane";
 import { repoData } from "./lib/data";
+import { RepoStats } from "@/types/types";
 
 function App() {
   const repositories = repoData.repositories;
-  const [selectedRepo, setSelectedRepo] = useState(repositories[0]);
+  const [selectedRepo, setSelectedRepo] = useState<RepoStats | null>(repositories[0]);
 
   const displayedDataPane = selectedRepo ? (
     <RepositoryStatsDataPane repo={selectedRepo} />
