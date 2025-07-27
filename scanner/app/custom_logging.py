2cfb59610603e9ac2324b78d69daff8622781886
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-27 23:02:41 +0100

    Improve Exception Logging (#318)
    # Pull Request
    
    ## Description
    
    This pull request introduces a new environment variable for CI
    environments, updates logging behavior to improve debugging in CI, and
    refactors imports for better readability in
    `scanner/app/custom_logging.py`.
    
    ### CI Environment Support:
    *
    [`.github/workflows/deploy.yml`](diffhunk://#diff-28802fbf11c83a2eee09623fb192785e7ca92a3f40602a517c011b947a1822d3R25):
    Added the `CI` environment variable to the workflow configuration to
    explicitly indicate when the code is running in a CI environment.
    
    ### Logging Improvements:
    *
    [`scanner/app/custom_logging.py`](diffhunk://#diff-2969fbabdc62ea4c8f548798b159621726f4a21af17c919c834e0aa25bcf6ee6R22-R31):
    Updated the `set_up_custom_logging` function to use
    `structlog_dev.ConsoleRenderer` with plain traceback formatting when the
    `CI` environment variable is set, enabling more readable logs for
    debugging in CI environments.
    
    ### Code Readability:
    *
    [`scanner/app/custom_logging.py`](diffhunk://#diff-2969fbabdc62ea4c8f548798b159621726f4a21af17c919c834e0aa25bcf6ee6L4-R12):
    Refactored `structlog` imports to use multi-line formatting for improved
    readability.

--- scanner/app/custom_logging.py
+++ scanner/app/custom_logging.py
@@ -1,7 +1,15 @@
 from logging import DEBUG, INFO
 from os import getenv
 
-from structlog import configure, get_logger, make_filtering_bound_logger, stdlib
+from structlog import (
+    configure,
+    get_logger,
+    make_filtering_bound_logger,
+    stdlib,
+)
+from structlog import (
+    dev as structlog_dev,
+)
 
 logger: stdlib.BoundLogger = get_logger()
 
@@ -11,4 +19,14 @@ def set_up_custom_logging() -> None:
     level = INFO
     if getenv("INPUT_DEBUG", "false").lower() == "true":
         level = DEBUG
-    configure(wrapper_class=make_filtering_bound_logger(level))
+    if getenv("CI"):
+        configure(
+            processors=[
+                structlog_dev.ConsoleRenderer(
+                    exception_formatter=structlog_dev.plain_traceback
+                ),
+            ],
+            wrapper_class=make_filtering_bound_logger(level),
+        )
+    else:
+        configure(wrapper_class=make_filtering_bound_logger(level))
